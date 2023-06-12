d8377f7eb805ac069c0bcd56d813714bc1a5d3e3
Tobias Silva <tobiassilva@hotmail.it>
2023-06-12 12:49:42 -0300

    Project completed

---
+++ src/app/core/models/bio.ts
@@ -0,0 +1,8 @@
+export interface Bio {
+
+    firstName: string;
+    lastName: string;
+    about: string[];
+    intro: string[];
+
+}
---
+++ src/app/portfolio/projects/projects.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { mergeMap } from 'rxjs';
+import { HeaderService } from 'src/app/core/services/header.service';
+import { ProjectsService } from 'src/app/core/services/projects.service';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent {
+  constructor (private headerService: HeaderService, private projectsService: ProjectsService) {}
+
+  isHome$ = this.headerService.isHome();
+
+  projects$ = this.isHome$.pipe(
+    mergeMap(atHome => this.projectsService.getProjects(atHome))
+  );
+
+  respOptions = [
+    { viewClasses: 'd-none d-md-flex', displayInColumn: false, useSmallerHeadings: false, titleClasses: 'display-3' },
+    { viewClasses: 'd-flex d-md-none', displayInColumn: true, useSmallerHeadings: true, titleClasses: '' }
+  ];
+}
